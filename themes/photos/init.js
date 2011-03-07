24fef70ad579c1d1f55b77b4f519677eb88f2e20
Lee Semel <lee@semel.net>
2011-03-07 16:51:17 -0500

    Photo wall progress

--- themes/photos/init.js
+++ themes/photos/init.js
@@ -1,14 +1,21 @@
 Convowall.option({
     search: {
-        q:'http://instagr.am OR http://picplz.com'
+        q:'bieber twitpic'
     }
 })
 
-$.getScript('js/jquery.embedly.min.js');
+$.getScript('js/lib/jquery.embedly.min.js');
+$.getScript('js/lib/jquery.livequery.js');
 
 $(document).ready(function() {
-  $('a.embedly').live(function() {
-        $(this).embedly({maxWidth:600,'method':'after'});
+    $('a.embedly').livequery(function() {
+        var that = this;
+        var href = $(this).attr('href');
+        $.embedly(href,{maxWidth:250,maxHeight:250},function(oembed,dict) {
+            // url, width, height description, thumbnail_url
+            var img = $('<img></img>').attr('src',oembed.thumbnail_url).attr('width',oembed.thumbnail_width).attr('height',oembed.thumbnail_height);
+            $(that).html(img);
+        });
     });
     var text = ['This is the Photo theme for Convowall','Edit the theme to change this rotating text, the Twitter search query, and to insert your logo'];
     $('#logo-text').text(text[0]).fadeIn('slow');
